0b5660a5178bbc5cdd98ff7e38ded73a4a0c9397
Iago López <ilopezsmx2011@gmail.com>
2025-10-27 11:22:04 +0100

    [ADD] clean_pdf_on_upload suscribers

--- src/genweb6/organs/subscribers.py
+++ src/genweb6/organs/subscribers.py
@@ -1,7 +1,4 @@
-from zope.lifecycleevent.interfaces import IObjectRemovedEvent
-from zope.lifecycleevent.interfaces import IObjectCreatedEvent
-from Products.CMFCore.interfaces import IActionSucceededEvent
-
+from genweb6.core.subscribers import clean_pdf_on_upload
 from genweb6.organs.utils import get_settings_property
 from genweb6.organs.content.organgovern.organgovern import IOrgangovern
 from genweb6.organs.content.sessio.sessio import ISessio
@@ -42,3 +39,16 @@ def update_indicators_on_acord_deletion(obj, event):
 def update_indicators_on_acord_review_state_change(obj, event):
     update_indicators(
         obj, service=get_settings_property('service_id'), indicator='acord-n')
+
+
+def clean_pdf_on_upload_file(obj, event):
+    clean_pdf_on_upload(obj, 'visiblefile')
+    clean_pdf_on_upload(obj, 'hiddenfile')
+
+
+def clean_pdf_on_upload_acta(obj, event):
+    clean_pdf_on_upload(obj, 'acta')
+
+
+def clean_pdf_on_upload_annex(obj, event):
+    clean_pdf_on_upload(obj, 'file')
