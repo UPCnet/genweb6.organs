661dbdb16613f0ad03e94b2ee38ff6eb9edcd94d
Iago López <ilopezsmx2011@gmail.com>
2025-11-06 08:59:20 +0100

    [ADD] subscribers purge_cache_varnish_organs

--- src/genweb6/organs/subscribers.py
+++ src/genweb6/organs/subscribers.py
@@ -4,7 +4,9 @@ from genweb6.organs.firma_documental.utils import is_file_uploaded_to_gdoc
 from genweb6.organs.indicators.updating import (
     update_indicators,
     update_indicators_if_state)
-
+from genweb6.core.purge import purge_varnish_paths
+from genweb6.organs.content.organsfolder.organsfolder import IOrgansfolder
+from zope.ramcache import ram
 
 def update_indicators_on_organ_deletion(obj, event):
     update_indicators_if_state(
@@ -60,3 +62,32 @@ def clean_pdf_on_upload_annex(obj, event):
     """Limpia PDF del campo file si no está subido al gDOC."""
     if is_file_in_open_organ(obj) and not is_file_uploaded_to_gdoc(obj):
         clean_pdf_on_upload(obj, 'file')
+
+
+def purge_cache_varnish_organs(obj, event):
+    """Limpia la caché de varnish recorriendo todos los paths hasta IOrgansfolder."""
+    ram.caches.clear()
+    
+    paths = []
+    paths.append('_purge_all')
+    # current = obj
+    
+    # # Recorrer hacia arriba añadiendo paths hasta llegar a IOrgansfolder
+    # while current is not None:
+    #     # Añadir el path del objeto actual
+    #     try:
+    #         paths.append(current.absolute_url_path())
+    #     except AttributeError:
+    #         break
+        
+    #     # Si hemos llegado a IOrgansfolder, parar
+    #     if IOrgansfolder.providedBy(current):
+    #         break
+        
+    #     # Subir al padre
+    #     try:
+    #         current = current.aq_parent
+    #     except AttributeError:
+    #         break
+    
+    purge_varnish_paths(obj, paths)
